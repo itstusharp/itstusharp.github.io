78208d303bd83f0a60b3db515af98c21d3905836
itstusharp <itstusharp@gmail.com>
2024-08-10 22:55:52 +0530

    Added shell script to query weather data

---
+++ queryWeather.sh
@@ -0,0 +1,9 @@
+
+#!/bin/bash
+WEATHER_FILE=./_data/devPosts.json
+
+echo -n "" >$POSTS_FILE
+
+WEATHER_FILE=$(curl 'https://api.open-meteo.com/v1/forecast?latitude=18.516726&longitude=73.856255&current=temperature_2m')
+
+echo "$WEATHER_FILE" | jq >>$WEATHER_FILE
