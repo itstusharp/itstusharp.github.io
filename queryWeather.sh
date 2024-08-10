4aa033eb0ea94a7412c50656f7ac039094504c26
itstusharp <itstusharp@gmail.com>
2024-08-10 23:31:15 +0530

    fixed typo and script logic

--- queryWeather.sh
+++ queryWeather.sh
@@ -2,8 +2,8 @@
 #!/bin/bash
 WEATHER_FILE=./_data/Weather.json
 
-echo -n "" >$POSTS_FILE
+echo -n "" >$WEATHER_FILE
 
-WEATHER_FILE=$(curl 'https://api.open-meteo.com/v1/forecast?latitude=18.516726&longitude=73.856255&current=temperature_2m')
+WEATHER_DATA=$(curl 'https://api.open-meteo.com/v1/forecast?latitude=18.516726&longitude=73.856255&current=temperature_2m')
 
-echo "$WEATHER_FILE" | jq >>$WEATHER_FILE
+echo "$WEATHER_DATA" >> $WEATHER_FILE
