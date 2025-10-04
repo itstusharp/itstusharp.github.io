1a6e19411500315253a15a2f4c5d4254c308a2b9
Tushar Patil <itstusharp@gmail.com>
2025-10-04 10:28:40 +0530

    Delete queryWeather.sh
    Removed weather details

--- queryWeather.sh
+++
@@ -1,9 +0,0 @@
-
-#!/bin/bash
-WEATHER_FILE=./_data/Weather.json
-
-echo -n "" >$WEATHER_FILE
-
-WEATHER_DATA=$(curl 'https://api.open-meteo.com/v1/forecast?latitude=18.516726&longitude=73.856255&current=temperature_2m')
-
-echo "$WEATHER_DATA" >> $WEATHER_FILE
